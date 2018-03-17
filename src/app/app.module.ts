7d2397e93eeb1ce737816d1a25d0879aa5b3163c
David Sierra <david.sierra@innocv.com>
2018-03-17 18:49:32 +0100

    add new components and service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import {SharedModule} from './shared/shared.module';
 import {AppRoutingModule} from './app-routing.module';
+import {HttpClientModule} from '@angular/common/http';
+import {DataService} from './shared/services/data.service';
 
 @NgModule({
   declarations: [
@@ -12,10 +14,11 @@ import {AppRoutingModule} from './app-routing.module';
   imports: [
     BrowserModule,
     SharedModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
 
   ],
-  providers: [],
+  providers: [DataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
