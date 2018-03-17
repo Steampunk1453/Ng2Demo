55c81e1f136c0b99f3eba159eb640ecce688bbb7
David Sierra <david.sierra@innocv.com>
2018-03-17 18:48:09 +0100

    move this elements

--- src/app/shared/productor/productor.pipe.ts
+++
@@ -1,8 +0,0 @@
-import {Pipe, PipeTransform} from '@angular/core';
-
-@Pipe({name: 'producer'})
-export class ProducerPipe implements PipeTransform {
-  transform(value: number): string {
-    return 'Product: ' + value.toString();
-  }
-}
