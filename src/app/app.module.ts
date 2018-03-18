e6269f7bdb94be02f0432f5657722144e6cd36b5
David Sierra <david.sierra@innocv.com>
2018-03-18 15:30:58 +0100

    add imports and modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,27 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
 import {SharedModule} from './shared/shared.module';
 import {AppRoutingModule} from './app-routing.module';
 import {HttpClientModule} from '@angular/common/http';
 import {DataService} from './shared/services/data.service';
+import { DetailComponent } from './product/detail/detail.component';
+import {ProductModule} from './product/product.module';
+import {LoginModule} from './login/login.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DetailComponent
   ],
   imports: [
     BrowserModule,
     SharedModule,
     AppRoutingModule,
-    HttpClientModule
-
+    HttpClientModule,
+    ProductModule,
+    LoginModule
   ],
   providers: [DataService],
   bootstrap: [AppComponent]
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,9 +3,9 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
 import { HeaderComponent } from './header/header.component';
-import { ListComponent } from './list/list.component';
-import {ProducerPipe} from './product/product.pipe';
-import {ProductService} from './product/shared/product.service';
+import { ListComponent } from '../product/list/list.component';
+import {ProducerPipe} from './product.pipe';
+import {ProductService} from '../product/shared/product.service';
 import {DataService} from './services/data.service';
 import {AuthService} from './services/auth.service';
 
