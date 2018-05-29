234557580fa43054ea2c4ed52712e72ef78102ab
Steampunk1453 <dasies11@gmail.com>
2018-05-29 22:37:18 +0200

    Add emit event for header

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,11 +1,23 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {ProductService} from "../../product/shared/product.service";
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
   // @Input() title: string = 'Default title';
+  isVisible = false;
+
+  constructor(
+    private _productService: ProductService) {
+  }
+
+  ngOnInit() {
+    this._productService.change.subscribe(isOpen => {
+      this.isVisible = isOpen;
+    });
+  }
 }
 
